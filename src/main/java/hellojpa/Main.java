47f58726ead0d031929d28d60565c3401b70e311
naegonggae <tkdtkd97@naver.com>
2023-06-12 17:06:32 +0900

    :memo: 조인

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -25,24 +25,35 @@ public class Main {
 
 		try {
 
-			for (int i = 0; i<100 ; i++) {
-				Member member = new Member();
-				member.setUsername("member"+i);
-				member.setAge(i);
-				em.persist(member);
-			}
-			// desc 내림차순
-			// order by 정렬기능 디폴트 오름차순
-			List<Member> result = em.createQuery("select m from Member m order by m.age desc",
-							Member.class)
-					.setFirstResult(1)
-					.setMaxResults(10)
-					.getResultList();
-			System.out.println("result = " + result.size());
-			for (Member member1 : result) {
-				System.out.println("member1 = " + member1);
+			Team team = new Team();
+			team.setName("teamA");
+			em.persist(team);
 
-			}
+			Member member = new Member();
+			member.setUsername("teamA");
+			member.setAge(10);
+			member.setTeam(team);
+			em.persist(member);
+
+			em.flush();
+			em.clear();
+
+			// inner join 에서 inner 생략가능
+			String query1 = "select m from Member m join m.team t";
+			// left outer join 에서 outer 생략가능
+			String query2 = "select m from Member m left join m.team t";
+			// 세타 조인
+			String query3 = "select m from Member m, Team t where m.username = t.name";
+			// 조인 대상 필터링
+			String query4 = "select m from Member m left join m.team t on t.name = 'teamA'";
+			// 연관관계 없는 엔티티 외부 조인
+			String query5 = "select m from Member m left join Team t on m.username = t.name";
+
+			List<Member> result = em.createQuery(query5, Member.class)
+					.getResultList();
+			// 여기서 team select 하는 쿼리는 LAZY 로 안해서 나감
+
+			System.out.println("result = " + result.size());
 
 			tx.commit(); // 트랜잭션 종료
 		} catch (Exception e) {
