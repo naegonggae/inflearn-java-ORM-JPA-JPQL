84353626b89442141b0af5d8a769975ea0465537
naegonggae <tkdtkd97@naver.com>
2023-06-13 14:01:35 +0900

    :memo: 페치 조인 1 - 기본

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -53,21 +53,23 @@ public class Main {
 			em.clear();
 			String query1 = "select m from Member m";
 			// 여기는 Team 이 프록시로 담겨서 온다.
-			String query2 = "select m from Member m join fetch m.team";
+			String query2 = "select distinct t from Team t join fetch t.members";
+			// distinct 는 중복되는 엔티티를 제거 해줌 -> 데이터 뻥튀기를 안하고 싶다하면 사용
 			// 이렇게하면 Team 은 프록시로 담기지 않고 실제 데이터로 담겨온다.
 			// join 앞에 left 붙이면 outer 조인
 			// 실무에서 엄청 쓴다고 함
-			List<Member> result = em.createQuery(query2, Member.class)
+			String query3 = "select t from Team t join t.members m";
+			// 초기에 팀정보만 불러옴 그다음 멤버 정보 불러오고 함
+			// 일반조인과 패치 조인의 차이
+			List<Team> result = em.createQuery(query3, Team.class)
 					.getResultList();
 
-			for (Member member : result) {
-				System.out.println("member = " + member.getUsername() +", "+ member.getTeam().getName());
-				// 회원1, 팀A(SQL)
-				// 회원2, 팀A(1차 캐시)
-				// 회원3, 팀B(SQL)
-				// select 문이 최초 member 하나, 팀A 가져올때 하나, 팀B 가져올때 하나 해서 세번 출력됐다.
-				// 만약 회원 100명이라면 -> N + 1
-			}
+			for (Team team : result) {
+				System.out.println("team = " + team.getName() +", "+ team.getMembers().size());
+				// 데이터 뻥튀기 된다. 일대다가 그럼 / 다대일은 뻥튀기 걱정없음
+				// 팀은 A랑 B 이렇게 두개밖에 없지만 팀A에 해당하는 멤버가 두명이기때문에 db에 들어갈때는
+				// 팀A-회원1 / 팀A-회원2 / 팀B-회원3 해서 3개가 된다는 말이다.
+ 			}
 
 			tx.commit(); // 트랜잭션 종료
 		} catch (Exception e) {
